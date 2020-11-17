782dc5cedb094e049441204f944466877b8b455f
Ryan Rodemoyer <ryan.rodemoyer@mortgagecadence.com>
2020-11-17 12:55:44 -0700

    api_v1 updates

---
+++ README.md
@@ -0,0 +1,50 @@
+# Writing Testable Code
+
+## La.S.I.C
+*Helping to see clearly in to writing testable code.*
+
+**L**oosely couple code from entry point. \
+**S**imultaneously write code and write test. \
+**I**solate dependencies using classes and interfaces. \
+**C**onstructors are for dependency initialization.
+
+### Testing Commandments
+1. Loosely couple your code from the entry point.
+	- Entry points are consoles, apis, services, GUI's, etc.
+		- The entry point always performs some type of setup or initialization.
+2. Write your code and the tests at approximately the same time.
+	- Will quickly discover what designer patterns are easy to easy, and which suck to test.
+3. Isolate dependencies using classes and interfaces.
+	- Use classes and interfaces to abstract/isolate/wrap away behavior with dependencies.
+4. Constructors are for dependency initialization, **not behavior initialization**!
+
+### Testing Suggestions
+
+5. Minimize magic dependencies.
+	- Magic Dependencies are often created during entry point setup and there's generally no way to ascertain this setup until an error occurs.
+6. Every language has gotchas. 
+    - .NET languages can be challenging to mock and fake.
+
+
+### What are dependencies?
+- Databases, file systems, FTP servers, network connections, etc.
+
+
+The constructor of an entry point is to initialize dependencies.
+The constructor of a model/POCO is to allow the creation of valid instances.
+	Or use the factory pattern to create instances.
+
+
+## Domain Chekr
+
+*Demonstrating the principals of LaSIC through a pseudo-application.*
+
+Domain Chekr is serivce to provide safety reports of domain names. We maintain a crawler constantly traversing the internet attempting to determine if a domain is safe to visit or is it has been compromised through some form of spam, malware or ransomware.
+
+Domain Chekr provides a web api for subscribers to get the latest report card for any given domain. The `/chekr` routes accepts a query parameter `domain`.
+
+Subscribers are provided with an api key that is required for all calls in to the Domain Chekr system. The api key is included as a header with the name `x-chekr-api-key`.
+
+Subscribers have rate limits that are tied to the api key. The rate limiter works on a rolling 60 second window. An error is returned if the calls exceed the established rate limit.
+
+Successfully validated calls then check the `domain` query param to get the input. Subscribers can send the data as the bare domain (ex. microsoft.com) or fully loaded (https://www.microsoft.com/page/something.aspx). The call is considered validated when it has a valid api key and is not rate limited.
--- src/api_v1/Controllers/ChekrController.cs
+++ src/api_v1/Controllers/ChekrController.cs
@@ -2,10 +2,11 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Primitives;
 
 namespace api_v1.Controllers
@@ -31,7 +32,7 @@ namespace api_v1.Controllers
         public int Id { get; set; }
 
         public string Domain { get; set; }
-        
+
         public DateTime LastScanned { get; set; }
 
         public ThreatVector ThreatVector { get; set; }
@@ -47,7 +48,10 @@ namespace api_v1.Controllers
 
     public enum ThreatVector
     {
-        None=1,Spam=2,Malware=3,Ransomware=4,
+        None = 1,
+        Spam = 2,
+        Malware = 3,
+        Ransomware = 4,
     }
 
     public class WebCall
@@ -63,6 +67,8 @@ namespace api_v1.Controllers
     public class ChekrResponse
     {
         public int CallsRemaining { get; set; }
+
+        public string ScanStatus { get; set; }
 
         public string SafetyStatus { get; set; }
 
@@ -82,27 +88,126 @@ namespace api_v1.Controllers
         public CheckrController(ILogger<CheckrController> logger)
         {
             _logger = logger;
+
+            LoadApiKeys().GetAwaiter().GetResult();
         }
 
         private static readonly Dictionary<string, ApiKey> ApiKeys = new Dictionary<string, ApiKey>
         {
-            { "asdf", new ApiKey(1, "adsf", 10)}
+            {"asdf", new ApiKey(1, "adsf", 10)}
             , {"qwer", new ApiKey(2, "qwer", 100)}
             , {"zxcv", new ApiKey(3, "zxcv", 5)}
         };
 
+        private List<ApiKey> _apiKeys;
+
+        private Dictionary<string, DomainEntry> _domains
+        {
+            get { return GetDomains().GetAwaiter().GetResult(); }
+        }
+
         private static readonly Dictionary<string, DomainEntry> Domains = new Dictionary<string, DomainEntry>
         {
-            { "yahoo.com", new DomainEntry(1,"yahoo.com", new DateTime(2020,11,6, 0, 0, 5), ThreatVector.None) }
-            , { "twitter.com", new DomainEntry(1,"twitter.com", new DateTime(2020,11,4, 0, 2, 12), ThreatVector.None) }
-            , { "microsoft.com", new DomainEntry(1,"microsoft.com", new DateTime(2020,11,16, 0, 5, 12), ThreatVector.None) }
-            , { "phishme.net", new DomainEntry(1,"phishme.net", new DateTime(2020,11,6, 1, 5, 29), ThreatVector.Spam) }
-            , { "clickjack.net", new DomainEntry(1,"clickjack.net", new DateTime(2020,11,6, 0, 17, 58), ThreatVector.Ransomware) }
-            , { "mlwarebites.com", new DomainEntry(1,"mlwarebites.com", new DateTime(2020,11,1, 0, 17, 58), ThreatVector.Malware) }
-            
+            {"yahoo.com", new DomainEntry(1, "yahoo.com", new DateTime(2020, 11, 6, 0, 0, 5), ThreatVector.None)}
+            , {"twitter.com", new DomainEntry(2, "twitter.com", new DateTime(2020, 11, 4, 0, 2, 12), ThreatVector.None)}
+            , {"microsoft.com", new DomainEntry(3, "microsoft.com", new DateTime(2020, 11, 16, 0, 5, 12), ThreatVector.None)}
+            , {"phishme.net", new DomainEntry(4, "phishme.net", new DateTime(2020, 11, 6, 1, 5, 29), ThreatVector.Spam)}
+            , {"clickjack.net", new DomainEntry(5, "clickjack.net", new DateTime(2020, 11, 6, 0, 17, 58), ThreatVector.Ransomware)}
+            , {"mlwarebites.com", new DomainEntry(6, "mlwarebites.com", new DateTime(2020, 11, 1, 0, 17, 58), ThreatVector.Malware)}
         };
 
         private static Dictionary<string, List<WebCall>> RateLimiter = new Dictionary<string, List<WebCall>>();
+
+        private async Task LoadApiKeys()
+        {
+            using var sql = new SqliteConnection("Data Source=data.db");
+            await sql.OpenAsync();
+
+            using var cmd = sql.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Id,ApiKeyName,CallsWithinOneMinute from cfgApiKeys";
+
+            using var reader = await cmd.ExecuteReaderAsync();
+
+            var results = new List<ApiKey>();
+
+            int idOrdinal = reader.GetOrdinal("Id");
+            int nameOrdinal = reader.GetOrdinal("ApiKeyName");
+            int limitOrdinal = reader.GetOrdinal("CallsWithinOneMinute");
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(idOrdinal);
+                string name = reader.GetString(nameOrdinal);
+                int limit = reader.GetInt32(limitOrdinal);
+
+                var key = new ApiKey(id, name, limit);
+                results.Add(key);
+            }
+
+            _apiKeys = results;
+        }
+        
+        private async Task<Dictionary<string,DomainEntry>> GetDomains()
+        {
+            using var sql = new SqliteConnection("Data Source=data.db");
+            await sql.OpenAsync();
+
+            using var cmd = sql.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Id,DomainName,LastScannedDate,ThreatVector from cfgdomains";
+
+            using var reader = await cmd.ExecuteReaderAsync();
+
+            var results = new Dictionary<string, DomainEntry>();
+
+            int idOrdinal = reader.GetOrdinal("Id");
+            int nameOrdinal = reader.GetOrdinal("DomainName");
+            int scannedOrdinal = reader.GetOrdinal("LastScannedDate");
+            int threatOrdinal = reader.GetOrdinal("ThreatVector");
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(idOrdinal);
+                string domainName = reader.GetString(nameOrdinal);
+                DateTime lastScannedDate = reader.GetDateTime(scannedOrdinal);
+                ThreatVector threatVector = (ThreatVector) reader.GetInt32(threatOrdinal);
+
+                var key = new DomainEntry(id, domainName, lastScannedDate, threatVector);
+                results.Add(domainName, key);
+            }
+
+            //_domains = results;
+
+            return results;
+        }
+
+        [HttpGet("upload")]
+        public async Task<IActionResult> UploadAsync()
+        {
+            using var sql = new SqliteConnection("Data Source=data.db");
+            await sql.OpenAsync();
+
+            using var deletecmd = sql.CreateCommand();
+            deletecmd.CommandText = "delete from cfgDomains";
+            deletecmd.ExecuteNonQuery();
+
+            using var cmd = sql.CreateCommand();
+            cmd.CommandText = "insert into cfgDomains (Id,DomainName,LastScannedDate,ThreatVector) values (@1,@2,@3,@4)";
+
+            foreach (var (_, value) in Domains)
+            {
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@1", value.Id);
+                cmd.Parameters.AddWithValue("@2", value.Domain);
+                cmd.Parameters.AddWithValue("@3", value.LastScanned);
+                cmd.Parameters.AddWithValue("@4", value.ThreatVector);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            return Ok();
+        }
 
         [HttpGet]
         public IActionResult GetAsync([FromQuery] string domain)
@@ -114,7 +219,7 @@ namespace api_v1.Controllers
 
                 bool apiKeyExists = ApiKeys.TryGetValue(apiKey, out ApiKey keydef);
                 if (apiKeyExists)
-                { 
+                {
                     bool exists2 = RateLimiter.TryGetValue(apiKey, out List<WebCall> calls);
                     if (!exists2)
                     {
@@ -139,7 +244,8 @@ namespace api_v1.Controllers
                             return BadRequest("domain input malformed");
                         }
 
-                        bool domainExists = Domains.TryGetValue(domain, out DomainEntry entry);
+                        bool domainExists = _domains.TryGetValue(domain, out DomainEntry entry);
+                        //bool domainExists = Domains.TryGetValue(domain, out DomainEntry entry);
                         if (domainExists)
                         {
                             string safetyStatus = null;
@@ -162,11 +268,12 @@ namespace api_v1.Controllers
 
                             var r = new ChekrResponse
                             {
-                                CallsRemaining = keydef.MaxCallsWithinOneMinute - recent.Count()
-                                , Domain = domain
-                                , SafetyStatus = safetyStatus
-                                , Threat = safetyStatus == "unknown" ? null : entry.ThreatVector.ToString()
-                                , PreviousThreat = safetyStatus == "unknown" ? entry.ThreatVector.ToString() : null
+                                CallsRemaining = keydef.MaxCallsWithinOneMinute - recent.Count(),
+                                Domain = domain,
+                                ScanStatus = "complete",
+                                SafetyStatus = safetyStatus,
+                                Threat = safetyStatus == "unknown" ? null : entry.ThreatVector.ToString(),
+                                PreviousThreat = safetyStatus == "unknown" ? entry.ThreatVector.ToString() : null
                             };
 
                             return Ok(r);
@@ -175,11 +282,12 @@ namespace api_v1.Controllers
                         {
                             var r = new ChekrResponse
                             {
-                                CallsRemaining = keydef.MaxCallsWithinOneMinute - recent.Count()
-                                , Domain = domain
-                                , SafetyStatus = "not-tracked"
-                                , Threat = null
-                                , PreviousThreat = null
+                                CallsRemaining = keydef.MaxCallsWithinOneMinute - recent.Count(),
+                                Domain = domain,
+                                ScanStatus = "in-process",
+                                SafetyStatus = "unknown",
+                                Threat = null,
+                                PreviousThreat = null
                             };
 
                             return Ok(r);
@@ -187,38 +295,8 @@ namespace api_v1.Controllers
                     }
                 }
             }
-            
+
             return Unauthorized();
         }
-    } 
-    
-    //[ApiController]
-    //[Route("[controller]")]
-    //public class WeatherForecastController : ControllerBase
-    //{
-    //    private static readonly string[] Summaries = new[]
-    //    {
-    //        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    //    };
-
-    //    private readonly ILogger<WeatherForecastController> _logger;
-
-    //    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    //    {
-    //        _logger = logger;
-    //    }
-
-    //    [HttpGet]
-    //    public IEnumerable<WeatherForecast> Get()
-    //    {
-    //        var rng = new Random();
-    //        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //        {
-    //            Date = DateTime.Now.AddDays(index),
-    //            TemperatureC = rng.Next(-20, 55),
-    //            Summary = Summaries[rng.Next(Summaries.Length)]
-    //        })
-    //        .ToArray();
-    //    }
-    //}
+    }
 }
