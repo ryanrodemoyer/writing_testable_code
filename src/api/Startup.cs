7926af781b929b79b635a1507f6b41801cc7af48
Ryan Rodemoyer <ryan.rodemoyer@mortgagecadence.com>
2020-11-18 08:47:14 -0700

    add dependencies to service container

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Controllers.v2;
 
 namespace api_v1
 {
@@ -26,6 +27,13 @@ namespace api_v1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDataAccess, DataAccess>();
+            services.AddScoped<IApiKeyRetriever, HttpHeaderApiKeyRetriever>();
+            services.AddScoped<IRateLimit, OneMinuteRateLimit>();
+            services.AddScoped<ITimeProvider, DotNetTimeProvider>();
+            services.AddScoped<DomainChekr>();
+
+            services.AddHttpContextAccessor();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
